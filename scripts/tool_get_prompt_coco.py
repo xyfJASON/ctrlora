24247f6c05b9515adad174f9088f910a0a30e621
xyfJASON <xyfjason@163.com>
2024-08-19 13:48:06 +0800

    Update tool_get_prompt_coco.py

--- scripts/tool_get_prompt_coco.py
+++ scripts/tool_get_prompt_coco.py
@@ -6,15 +6,23 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--ann_file', type=str, required=True)
     parser.add_argument('--save_path', type=str, required=True)
+    parser.add_argument('--ann_person_file', type=str, default=None, help='filter person images')
     args = parser.parse_args()
 
     with open(args.ann_file, 'r') as f:
         data = json.load(f)
 
+    image_ids = set(data['annotations'][i]['image_id'] for i in range(len(data['annotations'])))
+    if args.ann_person_file is not None:
+        with open(args.ann_person_file, 'r') as f:
+            data_person = json.load(f)
+        image_ids_person = set(data_person['annotations'][i]['image_id'] for i in range(len(data_person['annotations'])))
+        image_ids = image_ids & image_ids_person
+
     captions = dict()
     for i in range(len(data['annotations'])):
         filename = str(data['annotations'][i]['image_id']).zfill(12) + '.jpg'
-        if filename not in captions:
+        if filename not in captions and data['annotations'][i]['image_id'] in image_ids:
             captions[filename] = data['annotations'][i]['caption']
     captions = {k: v for k, v in sorted(captions.items())}
 
